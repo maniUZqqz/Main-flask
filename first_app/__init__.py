ff553a1cf7916a6d0df305ade95d399528ebc4d7
maniUZqqz <154362641+maniUZqqz@users.noreply.github.com>
2024-06-26 14:46:12 -0800

    m

--- first_app/__init__.py
+++ first_app/__init__.py
@@ -1,2 +1,18 @@
 from first_app import views, models, error_pages, templates, static
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "mani"
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
